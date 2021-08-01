e2fa6fa09841f7da011a4611f5cc858976940dac
VeriitoGA <87164514+VeriitoGA@users.noreply.github.com>
2021-08-01 16:36:38 -0500

    Create CtrlDocentes.js

---
+++ js/CtrlDocentes.js
@@ -0,0 +1,97 @@
+// @ts-nocheck
+import {
+  getAuth,
+  getFirestore
+} from "../lib/fabrica.js";
+import {
+  cod,
+  muestraError
+} from "../lib/util.js";
+import {
+  tieneRol
+} from "./seguridad.js";
+
+/** @type {HTMLUListElement} */
+const lista = document.
+  querySelector("#lista");
+const daoAlumno =
+  getFirestore().
+    collection("Alumno");
+
+getAuth().
+  onAuthStateChanged(
+    protege, muestraError);
+
+/** @param {import(
+    "../lib/tiposFire.js").User}
+    usuario */
+async function protege(usuario) {
+  if (tieneRol(usuario,
+    ["Administrador"])) {
+    consulta();
+  }
+}
+
+function consulta() {
+  daoAlumno.
+    orderBy("nombre")
+    .onSnapshot(
+      htmlLista, errConsulta);
+}
+
+/**
+ * @param {import(
+    "../lib/tiposFire.js").
+    QuerySnapshot} snap */
+function htmlLista(snap) {
+  let html = "";
+  if (snap.size > 0) {
+    snap.forEach(doc =>
+      html += htmlFila(doc));
+  } else {
+    html += /* html */
+      `<li class="vacio">
+        -- No hay alumnos
+        registrados. --
+      </li>`;
+  }
+  lista.innerHTML = html;
+}
+
+/**
+ * @param {import(
+    "../lib/tiposFire.js").
+    DocumentSnapshot} doc */
+function htmlFila(doc) {
+  /**
+   * @type {import("./tipos.js").
+                  Docente} */
+  const data = doc.data();
+  const nafilacion = cod(data.nafilacion);
+  const nombre = cod(data.nombre);
+  const materia = cod(data.materia);
+  const turno = cod(data.turno);
+  var fechan= cod(data.fechaalta);
+  var fechaalta = new Date(fechan);
+  var espacio="[   -   ]";
+  var dformat = [fecha.getDate()+1, fecha.getMonth()+1, fecha.getFullYear()].join('/');
+  const parámetros =
+    new URLSearchParams();
+  parámetros.append("id", doc.id);
+  return ( /* html */
+    `<li>
+      <a class="fila" href=
+  "docente.html?${parámetros}">
+        <strong class="primario">
+          ${nafilacion} ${nombre} ${materia} ${turno} ${dformat} 
+        </strong>
+      </a>
+     
+    </li>`);
+}
+
+/** @param {Error} e */
+function errConsulta(e) {
+  muestraError(e);
+  consulta();
+}
