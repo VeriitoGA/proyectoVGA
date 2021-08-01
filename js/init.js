3f0219a3f5e0cf2153cc5abb01573a99fc7b9eff
VeriitoGA <87164514+VeriitoGA@users.noreply.github.com>
2021-08-01 12:10:02 -0500

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,19 @@
+  
+/*  Inicializa Firebase con la
+ * configuración del proyecto.
+ * Revisa la configuración en tu
+ * servidor de Firebase y cópiala
+ * aquí sustituyendo los
+ * asteriscos. Los campos deben
+ * quedar igual que en tu
+ * servidor. */
+// @ts-ignore
+firebase.initializeApp({
+    apiKey: "AIzaSyBNRokOtxjhrJ-pkcx_OTQXDtIh84A4zJ0",
+    authDomain: "proyectovga.firebaseapp.com",
+    projectId: "proyectovga",
+    storageBucket: "proyectovga.appspot.com",
+    messagingSenderId: "324562799270",
+    appId: "1:324562799270:web:339c0f9895a38675268267",
+    measurementId: "G-3TF6EETNW8"
+});
